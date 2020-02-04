b49ed524186754359c5d9310a420a2103c04b7d7
FrederiqueRam <fb-ram@outlook.com>
2020-02-05 00:32:47 +0100

    Simulatie bijna af!!!! Gaat nog fout bij plot voor alle

--- Simulation Study Rosa Update.R
+++ Simulation Study Rosa Update.R
@@ -10,6 +10,7 @@ simulateOutliers <- function (n, epsilon, R, perc, p) {
   # control parameters
   set.seed(10)
   eps <- matrix(epsilon, ncol = p, nrow = n)
+  eps_vector <- rep(epsilon,n)
   intercept <- 1
   b <- rep(1,p)
   coefficients <- c(intercept,b)
@@ -93,7 +94,7 @@ simulateOutliers <- function (n, epsilon, R, perc, p) {
     Y_oos <- X_oos%*%coefficients+rnorm(ceiling(n*perc), mean = 0, sd = 1)
     # change some points into bad leverage points
     X_BLP <- ifelse(pout < eps, rmvnorm(n, mean = rep(0,p), sigma = diag(p)), X)
-    Y_BLP <- ifelse(poutvalue < eps, intercept + X_BLP%*%-b + rnorm(n, mean = 0, sd = 1),Y)
+    Y_BLP <- ifelse(poutvalue < eps_vector, intercept + X_BLP%*%-b + rnorm(n, mean = 0, sd = 1),Y)
     df <- data.frame(Y_BLP,X_BLP)
     # compute estimators
     lm <- lm(Y_BLP ~ ., df)
@@ -103,20 +104,30 @@ simulateOutliers <- function (n, epsilon, R, perc, p) {
     plugin <- lmDetMCD(X_BLP,Y_BLP,alpha=0.5)
     beta_plugin <- plugin$coefficients
     # evaluate using RMSE
-    beta_true <- rbind(intercept, b)
+    beta_true <- coefficients
     RMSE_lm <- RMSE(beta_true, beta_ols)
     RMSE_lts <- RMSE(beta_true, beta_lts)
     RMSE_plugin <- RMSE(beta_true, beta_plugin)
-    
-    c(beta_ols,beta_lts,beta_plugin,RMSE_lm,RMSE_lts,RMSE_plugin)
+    # evaluate prediction performance using RMSE
+    lm_pred <- X_oos%*%beta_ols    
+    lts_pred <- X_oos%*%beta_lts
+    plugin_pred <- X_oos%*%beta_plugin
+    RMSE_lm_oos <- RMSE(Y_oos, lm_pred)
+    RMSE_lts_oos <- RMSE(Y_oos, lts_pred)
+    RMSE_plugin_oos <- RMSE(Y_oos, plugin_pred)
+    c(beta_ols,beta_lts,beta_plugin,RMSE_lm,RMSE_lts,RMSE_plugin,RMSE_lm_oos,RMSE_lts_oos,RMSE_plugin_oos)
   }) 
   
   sampleVO <- replicate(R, {
     # generate multivariate normal data
-    X <- rmvnorm(n, sigma = Sigma)
+    X <- rmvnorm(n,  mean = rep(20,nrow(Sigma)),sigma = Sigma)
     Y <- intercept + X%*%b + rnorm(n, mean = 0, sd = 1)
+    # generate 10% extra observations to use oos
+    X_oos <- rmvnorm(ceiling(n*perc), mean = rep(20,nrow(Sigma)),sigma = Sigma)
+    X_oos <- cbind(rep(1,ceiling(n*perc)),X_oos)
+    Y_oos <- X_oos%*%coefficients+rnorm(ceiling(n*perc), mean = 0, sd = 1)
     # change some points into vertical outliers
-    Y_VO <- ifelse(pout < eps, intercept + X%*%b + rnorm(n, mean = 40, sd = 1), Y)
+    Y_VO <- ifelse(poutvalue < eps_vector, intercept + X%*%b + rnorm(n, mean = 40, sd = 1), Y)
     df <- data.frame(Y_VO,X)
     # compute estimators
     lm <- lm(Y_VO ~ ., df)
@@ -126,28 +137,39 @@ simulateOutliers <- function (n, epsilon, R, perc, p) {
     plugin <- lmDetMCD(X,Y_VO,alpha=0.5)
     beta_plugin <- plugin$coefficients
     # evaluate using RMSE
-    beta_true <- rbind(intercept, b)
+    beta_true <- coefficients
     RMSE_lm <- RMSE(beta_true, beta_ols)
     RMSE_lts <- RMSE(beta_true, beta_lts)
     RMSE_plugin <- RMSE(beta_true, beta_plugin)
-    
-    c(beta_ols,beta_lts,beta_plugin,RMSE_lm,RMSE_lts,RMSE_plugin)
+    # evaluate prediction performance using RMSE
+    lm_pred <- X_oos%*%beta_ols    
+    lts_pred <- X_oos%*%beta_lts
+    plugin_pred <- X_oos%*%beta_plugin
+    RMSE_lm_oos <- RMSE(Y_oos, lm_pred)
+    RMSE_lts_oos <- RMSE(Y_oos, lts_pred)
+    RMSE_plugin_oos <- RMSE(Y_oos, plugin_pred)
+    c(beta_ols,beta_lts,beta_plugin,RMSE_lm,RMSE_lts,RMSE_plugin,RMSE_lm_oos,RMSE_lts_oos,RMSE_plugin_oos)
   })
   
   # generate sample contaminated with good leverage points, bad leverage points & vertical outliers
-  sampleContam <- replicate(R,{
+  sampleContam <- replicate(R,{ #HIER GAAT IETS MIS!!
     # generate multivariate normal data
-    X <- rmvnorm(n, sigma = Sigma)
-    Y <- intercept + X%*%b + rnorm(n, mean = 0, sd = 1)
+    X <- rmvnorm(n,  mean = rep(20,nrow(Sigma)),sigma = Sigma)
+    # generate 10% extra observations to use oos
+    X_oos <- rmvnorm(ceiling(n*perc), mean = rep(20,nrow(Sigma)),sigma = Sigma)
+    X_oos <- cbind(rep(1,ceiling(n*perc)),X_oos)
+    Y_oos <- X_oos%*%coefficients+rnorm(ceiling(n*perc), mean = 0, sd = 1)
+    
     eps <- eps/3
+    eps_vector <- eps_vector/3
     # replace some point by good leverage points
-    X_GLP <- ifelse(pout < eps, mvrnorm(n, mean = rep(40,p), sigma = diag(p)), X)
+    X_GLP <- ifelse(pout <= eps, rmvnorm(n, mean = rep(40,p), sigma = diag(p)), X)
     Y_GLP <- intercept + X_GLP%*%b + rnorm(n, mean = 0, sd = 1)
     # replace some points by bad leverage points
-    X_BLP <- ifelse(eps < pout & pout < 2*eps, mvrnorm(n, mean = rep(-40,p), sigma = diag(p)), X_GLP)
-    Y_BLP <- ifelse(eps < pout & pout < 2*eps, intercept + X_BLP%*%-b + rnorm(n, mean = 40, sd = 1), Y_GLP)
+    X_BLP <- ifelse(eps < pout & pout <= 2*eps, rmvnorm(n, mean = rep(40,p), sigma = diag(p)), X_GLP)
+    Y_BLP <- ifelse(eps_vector < poutvalue & poutvalue <= 2*eps_vector, intercept + X_BLP%*%-b + rnorm(n, mean = 0, sd = 1), Y_GLP)
     # replace some points by vertical outliers
-    Y_VO <- ifelse(2*eps < pout & pout < 3*eps, intercept + X_BLP%*%b + rnorm(n, mean = 20, sd = 1), Y_BLP)
+    Y_VO <- ifelse(2*eps_vector < poutvalue & poutvalue <= 3*eps_vector, intercept + X_BLP%*%b + rnorm(n, mean = 40, sd = 1), Y_BLP)
     Y <- Y_VO
     X <- X_BLP
     df <- data.frame(Y,X)
@@ -159,12 +181,18 @@ simulateOutliers <- function (n, epsilon, R, perc, p) {
     plugin <- lmDetMCD(X,Y,alpha=0.5)
     beta_plugin <- plugin$coefficients
     # evaluate using RMSE
-    beta_true <- rbind(intercept, b)
+    beta_true <- coefficients
     RMSE_lm <- RMSE(beta_true, beta_ols)
     RMSE_lts <- RMSE(beta_true, beta_lts)
     RMSE_plugin <- RMSE(beta_true, beta_plugin)
-    
-    c(beta_ols,beta_lts,beta_plugin,RMSE_lm,RMSE_lts,RMSE_plugin)
+    # evaluate prediction performance using RMSE
+    lm_pred <- X_oos%*%beta_ols    
+    lts_pred <- X_oos%*%beta_lts
+    plugin_pred <- X_oos%*%beta_plugin
+    RMSE_lm_oos <- RMSE(Y_oos, lm_pred)
+    RMSE_lts_oos <- RMSE(Y_oos, lts_pred)
+    RMSE_plugin_oos <- RMSE(Y_oos, plugin_pred)
+    c(beta_ols,beta_lts,beta_plugin,RMSE_lm,RMSE_lts,RMSE_plugin,RMSE_lm_oos,RMSE_lts_oos,RMSE_plugin_oos)
   })
   
   results <- list(
